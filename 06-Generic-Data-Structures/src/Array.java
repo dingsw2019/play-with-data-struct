5cac2dc051a0cfc5b0335765d87fb55cbc6f7647
liupengyu <26631931@qq.com>
2020-06-13 10:08:43 +0800

    数组学习完成

--- 06-Generic-Data-Structures/src/Array.java
+++ 06-Generic-Data-Structures/src/Array.java
@@ -1,6 +1,10 @@
 import java.util.Objects;
 
 /**
+ * 数组
+ * 优点：快速定位, 索引 O(1) 查找
+ * 缺点：添加,删除需要移动数据。数据量大时不可接受
+ *
  * 第一版数组, 只能存放整型. 但数组作为一种数据结构, 应该支持存放任意类型数据
  * 而可以存放任意类型的结构,包含用户自定义类型, 称为泛型。
  *
@@ -8,6 +12,71 @@ import java.util.Objects;
  *
  * 基本数据类型的类对象:
  * Boolean, Byte, Char, Short, Integer, Long, Float, Double
+ *
+ * 动态数组
+ *  长度为4的数组A: [66,88,99,100]
+ *  创建一个新数组B,长度是原来的2倍, [null,null,null,null,null,null,null,null]
+ *  将数组A中元素, 移动到数组B
+ *  将指针指向数组B, 系统会自动回收数组A
+ *
+ * 简单的时间复杂度
+ *  O(1), O(n), O(lgn), O(nlogn), O(n^2)
+ *  大O描述的是算法的运行时间和输入数据之间的关系
+ *
+ *  addLast(e)      O(1)
+ *  addFirst(e)     O(n)
+ *  add(index,e)    头部添加O(n), 尾部添加O(1), 综合来看, O(n/2) = O(n)
+ *  整体来看, 添加函数的时间复杂度是 O(n), 综合看就看最坏的情况, 所以是 O(n)
+ *
+ *  removeLast(e)   O(1)
+ *  removeFirst(e)  O(n)
+ *  remove(index,e) O(n/2) = O(n)
+ *  整体来看, 删除函数的时间复杂度是 O(n)
+ *
+ *  resize(n)       O(n)
+ *
+ *  set(index,e)    O(1)
+ *  get(index)      O(1)
+ *  contains(e)     O(n)
+ *  find(e)         O(n)
+ *
+ *  整体来看
+ *      增   O(n)
+ *      删   O(n)
+ *      改   已知索引 O(1), 未知索引 O(n)
+ *      查   已知索引 O(1), 未知索引 O(n)
+ *
+ *
+ * resize 的复杂度分析
+ *
+ *  添加操作由以下方法组成, 如果只有addLast, 那么添加的时间复杂度是O(1), 但是因为
+ *  resize是 O(n)的, 所以添加整体是 O(n)的, 但是 resize 不是每次都调用, 所以 O(n)并不合适
+ *  可以使用均摊复杂度计算
+ *      addLast(e)      O(1)
+ *      addFirst(e)     O(n)
+ *      add(index,e)    头部添加O(n), 尾部添加O(1), 综合来看, O(n/2) = O(n)
+ *      resize(n)       O(n)
+ *
+ *  均摊复杂度, 计算 resize 的时间复杂度
+ *      假设 capacity=8, 9次addLast操作, 触发1次resize,
+ *      resize总共进行了 17次基本操作(原数组8次取数据, 新数组9次添加数据)
+ *      平均每次 addLast操作, 进行2次基本操作 (17/9)
+ *
+ *      假设 capacity = n, n+1次addLast, 触发resize, 总共进行 2n+1次基本操作(n+n+1)
+ *      平均每次addLast操作, 进行2次基本操作 (2n+1)/(n+1)
+ *
+ *  如果数组添加只使用 addLast, 再加上均摊复杂度看 resize, 那么添加的时间复杂度是 O(1) 的
+ *
+ *
+ * 复杂度震荡
+ *   addLast 将数组从 10扩容到 20, 然后调用removeLast, 数组又从 20 变成 10了
+ *   这种反复容量边界来回扩容缩容的操作,就会导致添加和删除的时间复杂度变成 O(n)
+ *
+ *   出现这种问题的原因：removeLast 时 resize 过于着急 (Eager)
+ *   解决方案：remove时使用 Lazy 方式, 不要一超过限制就缩容, 给个缓冲区
+ *           比如, 当 size == capacity/4 时, 才将capacity减半.
+ *           此时 capacity 还有 一般空间无元素
+ *           将扩容与缩容的阈值, 拉开差距, 就可避免震荡
  *
  */
 public class Array<E> {
@@ -48,11 +117,15 @@ public class Array<E> {
     // 在指定索引添加元素
     public void add(int index, E e) {
 
-        if (size == data.length)
-            throw new IllegalArgumentException("Add failed. Array is full.");
-
         if (index < 0 || index > size)
             throw new IllegalArgumentException("Add failed. Require index >= 0 and index <= size.");
+
+        // 扩容, 扩容固定值,
+        // 固定值大,在总量小的场景太浪费
+        // 固定值小, 在总量大的场景频率太高
+        if (size == data.length) {
+            resize(2*data.length);
+        }
 
         // 移动index之后的元素
         for (int i = size-1; i >= index; i--) {
@@ -126,6 +199,11 @@ public class Array<E> {
 
         size--;
         data[size] = null;
+
+        // 缩容
+        if (size == (data.length / 4) && (data.length / 2) != 0)
+            resize(data.length/2);
+
         return ret;
     }
 
@@ -161,5 +239,13 @@ public class Array<E> {
         }
         res.append(']');
         return res.toString();
+    }
+
+    // 重新申请数组内存空间
+    private void resize(int newCapacity) {
+        E[] newData = (E[])new Object[newCapacity];
+        for (int i=0; i<size; i++)
+            newData[i] = data[i];
+        data = newData;
     }
 }
