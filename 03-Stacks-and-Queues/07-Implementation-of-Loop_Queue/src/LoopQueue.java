351ca3b3969a83bd71e0e2019dffa4de5f9e9704
liupengyu <26631931@qq.com>
2020-06-14 10:29:12 +0800

    栈和队列, 完成

--- 03-Stacks-and-Queues/07-Implementation-of-Loop_Queue/src/LoopQueue.java
+++ 03-Stacks-and-Queues/07-Implementation-of-Loop_Queue/src/LoopQueue.java
@@ -5,11 +5,17 @@
  * 空队, front == tail
  * 满队, (tail+1) % queue.length == front
  *
+ * 复杂度
+ *     void enqueue(E e);   O(1) 均摊
+ *     E dequeue();         O(1) 均摊
+ *     E getFront();        O(1)
+ *     int getSize();       O(1)
+ *     boolean isEmpty();   O(1)
  */
 public class LoopQueue<E> implements Queue<E> {
 
     private E[] data;
-    private int front, tail;
+    private int front,tail;
     private int size;
 
     public LoopQueue(int capacity) {
@@ -18,39 +24,40 @@ public class LoopQueue<E> implements Queue<E> {
         tail = 0;
         size = 0;
     }
-    
-    public LoopQueue() {
+
+    public LoopQueue(){
         this(10);
     }
-    
+
     public int getCapacity() {
         return data.length-1;
     }
-    
+
     @Override
     public boolean isEmpty() {
         return front == tail;
     }
-    
+
     @Override
     public int getSize() {
         return size;
     }
 
+
     @Override
     public void enqueue(E e) {
-        // 队列满了, 扩容
+        // 队列满了,扩容
         if ((tail+1) % data.length == front)
             resize(getCapacity()*2);
 
         data[tail] = e;
         tail = (tail+1) % data.length;
+
         size++;
     }
 
     @Override
     public E dequeue() {
-        // 队列空
         if (isEmpty())
             throw new IllegalArgumentException("Cannot dequeue from an empty queue.");
 
@@ -62,6 +69,7 @@ public class LoopQueue<E> implements Queue<E> {
         // 缩容
         if (size == getCapacity() / 4 && getCapacity()/2 != 0)
             resize(getCapacity()/2);
+
         return ret;
     }
 
@@ -73,11 +81,11 @@ public class LoopQueue<E> implements Queue<E> {
         return data[front];
     }
 
-    private void resize(int newCapacity) {
+    public void resize(int newCapacity) {
         E[] newData = (E[])new Object[newCapacity+1];
-        for (int i=0; i<size; i++) {
-            newData[i] = data[(i+front) % data.length];
-        }
+        for (int i = 0; i < size; i++)
+            newData[i] = data[(i+front)%data.length];
+
         data = newData;
         front = 0;
         tail = size;
@@ -88,12 +96,13 @@ public class LoopQueue<E> implements Queue<E> {
         StringBuilder res = new StringBuilder();
         res.append(String.format("Queue: size = %d, capacity = %d\n", size, getCapacity()));
         res.append("front [");
-        for (int i = front; i != tail; i = (i+1)%data.length) {
+        for (int i=front; i!=tail; i=(i+1)%data.length) {
             res.append(data[i]);
             if ((i+1)%data.length != tail)
                 res.append(", ");
         }
         res.append("] tail");
+
         return res.toString();
     }
 
