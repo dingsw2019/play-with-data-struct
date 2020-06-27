8f80193ea6b57d5bc9046da9e0e143f6a21a7e37
liupengyu <26631931@qq.com>
2020-06-27 18:25:15 +0800

    线段树学习完成

--- 09-Segment-Tree/05-Segment-Tree-Problems-in-Leetcode/src/Main.java
+++ 09-Segment-Tree/05-Segment-Tree-Problems-in-Leetcode/src/Main.java
@@ -4,10 +4,10 @@ public class Main {
 
         Integer[] nums = {-2, 0, 3, -5, 2, -1};
 
-        SegmentTree<Integer> segTree = new SegmentTree<>(nums, (a,b) -> a + b);
+        SegmentTree<Integer> segTree = new SegmentTree<>(nums,(a,b)->a + b);
+
 //        System.out.println(segTree);
 
-        // 范围查找
         System.out.println(segTree.query(0,2));
         System.out.println(segTree.query(2,5));
         System.out.println(segTree.query(0,5));
--- 09-Segment-Tree/05-Segment-Tree-Problems-in-Leetcode/src/SegmentTree.java
+++ 09-Segment-Tree/05-Segment-Tree-Problems-in-Leetcode/src/SegmentTree.java
@@ -1,45 +1,37 @@
-/**
- * 数组实现线段树
- * @param <E>
- */
 public class SegmentTree<E> {
 
-    private E[] data;
     private E[] tree;
+    private E[] data;
+
     private Merger<E> merger;
 
     public SegmentTree(E[] arr, Merger<E> merger){
-
         this.merger = merger;
+
         data = (E[])new Object[arr.length];
 
-        for (int i = 0; i < arr.length; i++)
+        for(int i = 0; i < arr.length; i++)
             data[i] = arr[i];
 
-        tree = (E[])new Object[4 * arr.length];
+        tree = (E[])new Object[4*arr.length];
         buildSegmentTree(0,0,arr.length-1);
     }
 
-    // 在数组 index 位置上存储 [l,r] 范围的结果
-    private void buildSegmentTree(int treeIndex, int l, int r) {
-        // 结束条件
+    // 以 treeIndex 为根, 构建 [l,r] 的线段树
+    private void buildSegmentTree(int treeIndex, int l, int r){
         if (l == r) {
             tree[treeIndex] = data[l];
             return;
         }
 
-        // 计算子节点索引位置
-        int leftChildIndex = leftChild(treeIndex);
-        int rightChildIndex = rightChild(treeIndex);
+        int leftTreeIndex = leftChild(treeIndex);
+        int rightTreeIndex = rightChild(treeIndex);
 
-        // 缩小范围, 递归构建线段树结构
-        int mid = l + (r - l) / 2;
-        buildSegmentTree(leftChildIndex,l,mid);
-        buildSegmentTree(rightChildIndex,mid+1,r);
+        int mid = l + (r-l)/2;
+        buildSegmentTree(leftTreeIndex,l,mid);
+        buildSegmentTree(rightTreeIndex,mid+1,r);
         
-        // 计算线段节点的值, 根据不同功能, 方法不同
-        tree[treeIndex] = merger.merge(tree[leftChildIndex],tree[rightChildIndex]);
-        
+        tree[treeIndex] = merger.merge(tree[leftTreeIndex],tree[rightTreeIndex]);
     }
 
     public int getSize(){
@@ -47,21 +39,19 @@ public class SegmentTree<E> {
     }
 
     public E get(int index){
-        if (index < 0 || index >= data.length)
-            throw new IllegalArgumentException("Index Illegal");
-
+        if(index < 0 || index >= data.length)
+            throw new IllegalArgumentException("Index is illegal.");
         return data[index];
     }
 
     private int leftChild(int index){
         return 2*index+1;
     }
-
     private int rightChild(int index){
         return 2*index+2;
     }
 
-    // 返回 [queryL, queryR] 区间的值, 递归算法
+    // 计算区间 [l,r] 的值
     public E query(int queryL, int queryR){
         if (queryL < 0 || queryL >= data.length ||
                 queryR < 0 || queryR >= data.length || queryL > queryR)
@@ -70,32 +60,58 @@ public class SegmentTree<E> {
         return query(0,0,data.length-1,queryL,queryR);
     }
 
-    // 在 threeIndex 为根的线段树中[l,r]的范围里, 查找区间 [queryL,queryR] 的值
+    // 以 treeIndex 为根在[l,r]内, 搜索[queryL,queryR]的值
     private E query(int treeIndex, int l, int r, int queryL, int queryR){
-        if (l == queryL && r == queryR)
+        if (l == queryL  && r ==queryR)
             return tree[treeIndex];
 
-        int mid = l + (r - l)/2;
+        int mid = l + (r-l)/2;
         int leftTreeIndex = leftChild(treeIndex);
         int rightTreeIndex = rightChild(treeIndex);
 
-        // 查找范围全在右子树
-        if (queryL >= mid + 1)
+        if (queryL >= mid+1)
             return query(rightTreeIndex,mid+1,r,queryL,queryR);
-        // 查找范围全在左子树
         else if (queryR <= mid)
             return query(leftTreeIndex,l,mid,queryL,queryR);
 
-        // 查找范围在左右子树
         E leftResult = query(leftTreeIndex,l,mid,queryL,mid);
         E rightResult = query(rightTreeIndex,mid+1,r,mid+1,queryR);
 
         return merger.merge(leftResult,rightResult);
     }
 
+    public void set(int index,E e){
+        if (index < 0 || index >= data.length)
+            throw new IllegalArgumentException("index Illegal");
+
+        data[index] = e;
+        set(0,0,data.length-1,index,e);
+    }
+    // 在以treeIndex为根的线段树中更新index的值为e
+    private void set(int treeIndex, int l, int r, int index, E e){
+        // 找到要修改的节点
+        if (l == r) {
+            tree[treeIndex] = e;
+            return;
+        }
+
+        int mid = l +(r-l)/2;
+
+        int leftTreeIndex = leftChild(treeIndex);
+        int rightTreeIndex = rightChild(treeIndex);
+
+        if (index >= mid+1)
+            set(rightTreeIndex,mid+1,r,index,e);
+        else // index <= mid
+            set(leftTreeIndex,l,mid,index,e);
+
+        // 更新父节点
+        tree[treeIndex] = merger.merge(tree[leftTreeIndex],tree[rightTreeIndex]);
+    }
+
     @Override
     public String toString(){
-        StringBuilder res = new StringBuilder();
+        StringBuilder res= new StringBuilder();
         res.append('[');
         for (int i = 0; i < tree.length; i++) {
             if (tree[i] != null)
